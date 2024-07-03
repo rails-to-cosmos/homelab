5414ded2f8e47267de5fd5ce63dd24484b11ad49
Dmitry Akatov <dmitry.akatov@protonmail.com>
2024-07-03 10:48:52 +0200

    package

---
+++ src/homelab/config/__init__.py
@@ -0,0 +1,4 @@
+from .symlink import SYMLINK_MAP
+from .fonts import FONTS_LOCATION
+
+__all__ = ["FONTS_LOCATION", "SYMLINK_MAP"]
---
+++ src/homelab/config/fonts.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+
+FONTS_LOCATION = Path.home() / "sync" / "resources" / "fonts"
